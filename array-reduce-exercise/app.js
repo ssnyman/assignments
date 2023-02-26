0ef5a3cb34e7f4c5e59e50135ea84b4456faf60e
Shelby Snyman <ssnymandev@gmail.com>
2023-02-26 07:43:53 -0700

    finished

--- array-reduce-exercise/app.js
+++ array-reduce-exercise/app.js
@@ -77,4 +77,55 @@ var arrays = [
 console.log(flatten(arrays)); // ["1", "2", "3", true, 4, 5, 6];
 
 
-// 6. 
+// 6. return object representing results of vote //
+var voters = [
+    {name:'Bob' , age: 30, voted: true},
+    {name:'Jake' , age: 32, voted: true},
+    {name:'Kate' , age: 25, voted: false},
+    {name:'Sam' , age: 20, voted: false},
+    {name:'Phil' , age: 21, voted: true},
+    {name:'Ed' , age:55, voted:true},
+    {name:'Tami' , age: 54, voted:true},
+    {name: 'Mary', age: 31, voted: false},
+    {name: 'Becky', age: 43, voted: false},
+    {name: 'Joey', age: 41, voted: true},
+    {name: 'Jeff', age: 30, voted: true},
+    {name: 'Zack', age: 19, voted: false}
+];
+
+function voterResults(arr) {
+    return arr.reduce((acc, cur) => {
+        if (cur.age <= 25 && cur.age >= 18 && cur.voted)
+            acc.numYoungVotes++
+        if (cur.age <= 25 && cur.age >= 18)
+            acc.numYoungPeople++
+        if (cur.age <= 35 && cur.age >= 26 && cur.voted)
+            acc.numMidVotesPeople++
+        if (cur.age <= 35 && cur.age >= 26)
+            acc.numMidsPeople++
+        if (cur.age <= 55 && cur.age >= 36 && cur.voted)
+            acc.numOldVotesPeople++
+        if (cur.age <= 55 && cur.age >= 36)
+            acc.numOldsPeople++
+        
+        return acc
+    }, {
+        numYoungVotes: 0,
+        numYoungPeople: 0,
+        numMidVotesPeople: 0,
+        numMidsPeople: 0,
+        numOldVotesPeople: 0,
+        numOldsPeople: 0
+    })
+}
+
+console.log(voterResults(voters)); // Returned value shown below:
+/*
+{ numYoungVotes: 1,
+numYoungPeople: 4,
+numMidVotesPeople: 3,
+numMidsPeople: 4,
+numOldVotesPeople: 3,
+numOldsPeople: 4
+}
+*/

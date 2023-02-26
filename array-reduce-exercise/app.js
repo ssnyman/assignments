03fb523c01cee9b0c908533ebe3f7dc9f727aaf4
Shelby Snyman <ssnymandev@gmail.com>
2023-02-26 07:21:27 -0700

    in progress updated

--- array-reduce-exercise/app.js
+++ array-reduce-exercise/app.js
@@ -1,4 +1,4 @@
-// turn array of numbers into total of all numbers //
+// 1. turn array of numbers into total of all numbers //
 function total(arr) {
     return arr.reduce((final, num) => {
         final += num
@@ -7,7 +7,8 @@ function total(arr) {
 }
 console.log(total([1, 2, 3])); // 6
 
-// array of numbers into string
+
+// 2. array of numbers into string
 function stringConcat(arr) { 
     return arr.reduce((acc, cur) => {
         return acc + "" + cur
@@ -15,4 +16,65 @@ function stringConcat(arr) {
 }
 console.log(stringConcat([1, 2, 3])); // "123"
 
-//
+
+// 3. turn an array of voter objects into count of how many voted //
+function totalVotes(arr) {
+    return arr.reduce(function (final, voter) {
+        if (voter.voted) {
+            final++
+        }
+        return final
+    }, 0)
+}
+
+var voters = [
+    {name:'Bob' , age: 30, voted: true},
+    {name:'Jake' , age: 32, voted: true},
+    {name:'Kate' , age: 25, voted: false},
+    {name:'Sam' , age: 20, voted: false},
+    {name:'Phil' , age: 21, voted: true},
+    {name:'Ed' , age:55, voted:true},
+    {name:'Tami' , age: 54, voted:true},
+    {name: 'Mary', age: 31, voted: false},
+    {name: 'Becky', age: 43, voted: false},
+    {name: 'Joey', age: 41, voted: true},
+    {name: 'Jeff', age: 30, voted: true},
+    {name: 'Zack', age: 19, voted: false}
+];
+console.log(totalVotes(voters)); // 7
+
+
+// 4. how much would it cose to buy everything at once //
+function shoppingSpree(arr) {
+    return arr.reduce(function (final, item) {
+        final = final + item.price
+        return final
+    }, 0)
+}
+
+var wishlist = [
+    { title: "Tesla Model S", price: 90000 },
+    { title: "4 carat diamond ring", price: 45000 },
+    { title: "Fancy hacky Sack", price: 5 },
+    { title: "Gold fidgit spinner", price: 2000 },
+    { title: "A second Tesla Model S", price: 90000 }
+];
+
+ console.log(shoppingSpree(wishlist)); // 227005
+
+
+// 5. given an array of arrays, turn into single array //
+function flatten(arr) {
+    return arr.reduce((acc, cur) => acc.concat(cur), [])
+}
+
+var arrays = [
+    ["1", "2", "3"],
+    [true],
+    [4, 5, 6]
+];
+
+console.log(flatten(arrays)); // ["1", "2", "3", true, 4, 5, 6];
+
+
+// 6. 

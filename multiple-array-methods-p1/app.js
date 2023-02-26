a6f574be6ec0d526c86f118532c822935b4ba1af
Shelby Snyman <ssnymandev@gmail.com>
2023-02-26 09:06:11 -0700

    in progress

---
+++ multiple-array-methods-p1/app.js
@@ -0,0 +1,48 @@
+var peopleArray = [
+    {
+        firstName: "Sarah",
+        lastName: "Palin",
+        age: 47
+    },
+    {
+        firstName: "Frank",
+        lastName: "Zappa",
+        age: 12
+    },
+    {
+        firstName: "Rick",
+        lastName: "Sanchez",
+        age: 78
+    },
+    {
+        firstName: "Morty",
+        lastName: "Smith",
+        age: 29
+    },
+    {
+        firstName: "Kyle",
+        lastName: "Mooney",
+        age: 27
+    },
+    {
+        firstName: "Pasha",
+        lastName: "Datsyuk",
+        age: 13
+    },
+    {
+        firstName: "Lev",
+        lastName: "Tolstoy",
+        age: 82
+    }
+]
+
+// 1. Returns list of everyone older than 18 //
+function sortedAge (arr) {
+    return arr.filter((adult) => {
+        if (adult.age > 18)
+            return adult
+    })
+}
+console.log(sortedAge(peopleArray));
+
+// 2. Sorted alphabetically and where //
